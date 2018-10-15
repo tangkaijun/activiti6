d08e842b293f068b67296d2a8c8904d831896c2e
tangkj <tangkj@hua-cloud.com.cn>
2018-10-15 20:38:53 +0800

    init

---
+++ activiti-api/src/main/java/com/kjtang/activiti/ActivitiApplication.java
@@ -0,0 +1,19 @@
+package com.kjtang.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by kjtang on 2018/10/15.
+ */
+@SpringBootApplication
+public class ActivitiApplication {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args){
+        SpringApplication.run(ActivitiApplication.class,args);
+    }
+
+}
---
+++ activiti-core/src/main/java/com/kjtang/activiti/ActivitiApplication.java
@@ -0,0 +1,12 @@
+package com.kjtang.activiti;
+
+/**
+ * Created by kjtang on 2018/10/15.
+ */
+public class ActivitiApplication {
+
+    public static void main(String[] args){
+        //bpmn-js
+        //SpringApplication.run();
+    }
+}
