8b1a8f2551cc46ade911b259a43e72e4da39c78f
kjtang <571856518@qq.com>
2018-10-15 23:57:44 +0800

    auto

--- activiti-api/src/main/java/com/kjtang/activiti/ActivitiApplication.java
+++ activiti-api/src/main/java/com/kjtang/activiti/ActivitiApplication.java
@@ -1,12 +1,13 @@
 package com.kjtang.activiti;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by kjtang on 2018/10/15.
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ActivitiApplication {
 
     /**
